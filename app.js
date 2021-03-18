767ac515a17d1a97e99c37751a0b941a1d1c088e
Istrate Mihai <i.mihai9960@gmail.com>
2021-03-18 20:47:24 +0200

    Added content to each file
    In style.css i've added color and font-size for the text and the button and developed the functionality for app.js such as setINterval to create the animation,the greet message appears and changes opacitiy from 0 to 1

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 document.addEventListener('DOMContentLoaded', ()=> {
 
+var greet=document.getElementById("btn");
+var hello=document.getElementsByTagName("h1")[0];
+var opac=0;
 
+greet.addEventListener('click',()=>{
 
+greet.style.display="none";
+var t=setInterval(Present,100);
 
-
-
-
+function Present(){
+   opac+=0.1;
+   if( opac==1){
     
+    clearInterval(t);
+       
+}else{
+     hello.style.opacity=opac;
+   }
+
+}
+
+});
+
+
+
+
+
+
 
 
 
